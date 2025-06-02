d151cfaec03f360272fd87536373f3718ced934e
PhillipThomas1992 <phillipthomas@live.de>
2025-06-02 15:09:40 +0200

    fix storeage config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 set -e
 
+# Create folder for temporary config
 mkdir -p /tmp/loki
-: > /tmp/loki/loki.yaml  # clear or create config
+: > /tmp/loki/loki.yaml  # clear or create config file
 
+# Extract config from env
 config=$(echo "$MAKE87_CONFIG" | jq -c '.config')
+storage_path=$(echo "$config" | jq -r '.storage_path // "/loki"')
+object_store=$(echo "$config" | jq -r '.object_store // "filesystem"')
 
+# Write Loki configuration
 cat <<EOF >> /tmp/loki/loki.yaml
 auth_enabled: false
 
@@ -23,7 +28,7 @@ schema_config:
   configs:
     - from: 2022-01-01
       store: boltdb-shipper
-      object_store: $(echo "$config" | jq -r '.object_store // "filesystem"')
+      object_store: ${object_store}
       schema: v11
       index:
         prefix: index_
@@ -31,16 +36,20 @@ schema_config:
 
 storage_config:
   boltdb_shipper:
-    active_index_directory: $(echo "$config" | jq -r '.storage_path // "/tmp/loki"')/index
-    cache_location: $(echo "$config" | jq -r '.storage_path // "/tmp/loki"')/index_cache
-    shared_store: $(echo "$config" | jq -r '.object_store // "filesystem"')
+    active_index_directory: ${storage_path}/index
+    cache_location: ${storage_path}/index_cache
+    shared_store: ${object_store}
 
   filesystem:
-    directory: $(echo "$config" | jq -r '.storage_path // "/tmp/loki"')/chunks
+    directory: ${storage_path}/chunks
 
 limits_config:
   retention_period: $(echo "$config" | jq -r '.retention_period // "7d"')
   max_entries_limit_per_query: $(echo "$config" | jq -r '.max_entries_limit_per_query // 5000')
+
+compactor:
+  working_directory: ${storage_path}/compactor
 EOF
 
+# Start Loki with the generated config
 exec loki -config.file=/tmp/loki/loki.yaml
